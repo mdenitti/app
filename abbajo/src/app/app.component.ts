699fa6dddda905b03ac6fc0a5a9ae54c66c2f56a
mdenitti <massimodn77@gmail.com>
2025-01-15 11:43:07 +0100

    fixed module vs component element in code

--- abbajo/src/app/app.component.ts
+++ abbajo/src/app/app.component.ts
@@ -1,35 +1,13 @@
 import { Component } from '@angular/core';
-
-
-
-=======
-//import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from '../app/components/registerform/registerform.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RegisterComponent],
-  template: `
-    <div class="app-container">
-      <h1>Angular 19 Users</h1>
-      <app-user-list />
-    </div>
-  `,
-  styles: [`
-    .app-container {
-      padding: 20px;
-    }
-  `]
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule]
 })
 export class AppComponent {
+  title = 'abbajo';
+filteredPosts: any;
 }
